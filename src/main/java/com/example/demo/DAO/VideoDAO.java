53c8e4644e84727b2d05cd1a719465974adc64f2
zhangcn <862459852@qq.com>
2020-04-14 20:51:00 +0800

    小黄 添加video功能

---
+++ src/main/java/com/example/demo/DAO/VideoDAO.java
@@ -0,0 +1,10 @@
+package com.example.demo.DAO;
+
+import com.example.demo.entity.Video;
+
+import java.util.List;
+
+public interface VideoDAO {
+
+    List<Video> queryAll();
+}
---
+++ src/main/java/com/example/demo/entity/Video.java
@@ -0,0 +1,8 @@
+package com.example.demo.entity;
+
+public class Video {
+
+    private String id;
+    private String path;
+    private String cover;
+}
